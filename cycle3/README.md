546795d232116a23683e3cb77536b9d009dfae75
DocChin <cisaache@gmail.com>
2021-06-06 17:28:43 -0700

    complete

--- cycle3/README.md
+++ cycle3/README.md
@@ -1,7 +1,12 @@
-# Cycle 3: Network Mapper with Visual
+# Cycle 3: Network Grapher
 Mapping a network is cool but mgmt needs a visual and non tech-folks work in spreadsheets.
-Might as well do all the things with a simple script.
+Might as well do all the things with a simple script. Then takes it a step further and scans open ports.
 ## Python modules
 - networkx
 - pandas
 - mathplotlib
+- nmap
+## Future Work
+- Incorporate nmap vulnerability scanning on hosts
+- Incorporate nmap scanning in graphing capability
+- Be "less noisy" =)
--- cycle3/network_mapper.py
+++ cycle3/network_mapper.py
@@ -1,23 +1,19 @@
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
-%matplotlib inline
+import nmap3
 
 # load data
-df = pd.read_csv("data/book1.csv")
-# pick only important weights (hard threshold)
-df = df.loc[df['weight']>10, :]
-df
+df = pd.read_csv("./network.csv")
 
-# import
-import networkx as nx
 # load pandas df as networkx graph
 G = nx.from_pandas_edgelist(df,
-                            source='Source',
-                            target='Target',
-                            edge_attr='weight')
-print("No of unique characters:", len(G.nodes))
-print("No of connections:", len(G.edges))
+                            source='SRC_FQDN',
+                            target='DEST_FQDN')
+print("No of unique servers:", len(G.nodes), "\n")
+for x in G.nodes:
+    print(x)
+print("\nNo of connections:", len(G.edges), "\n")
 
 # all graph options
 graphs_viz_options = [nx.draw, nx.draw_networkx, nx.draw_circular, nx.draw_kamada_kawai, nx.draw_random, nx.draw_shell, nx.draw_spring]
@@ -26,5 +22,28 @@ graphs_viz_options = [nx.draw, nx.draw_networkx, nx.draw_circular, nx.draw_kamad
 selected_graph_option = 0
 
 # plot
+print("Drawing Graph now...")
 plt.figure(figsize=(8,6), dpi=100)
-graphs_viz_options[selected_graph_option](G)
+graphs_viz_options[selected_graph_option](G, with_labels=True)
+plt.show()
+print("Saving figure as graph.png")
+plt.savefig("graph.png")
+
+
+print("\nScanning open ports on available servers...")
+for y in G.nodes:
+    nmap = nmap3.Nmap()
+    results_ports = nmap.scan_top_ports(y)
+    print(results_ports)
+
+
+print("\nScanning tcp ports on available servers...")
+nmap = nmap3.NmapScanTechniques()
+for z in G.nodes:
+    results_tcp = nmap.nmap_tcp_scan(z)
+    print(results_tcp)
+
+print("\nPinging on available server...")
+for a in G.nodes:
+    results_ping = nmap.nmap_ping_scan(a)
+    print(results_ping)
